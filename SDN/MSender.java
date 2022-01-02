c0a148a2ac0ee3c0444cb85d0efca9f6679cbf8b
Barand67 <96434748+Barand67@users.noreply.github.com>
2022-01-02 19:17:10 +0300

    Update MSender.java

--- SDN/MSender.java
+++ SDN/MSender.java
@@ -1,12 +1,12 @@
 package SDN;
 
-import Root.Sender;
+import Root.Sender; //paketi ekliyor
 
 
 public class MSender {
     
     public static void main(String[] args) {
-        Sender sender = new Sender(1000);
-        sender.start();
+        Sender sender = new Sender(1000);//yeni yollayıcı yapıyor
+        sender.start();//programı başlatıyor
     }
 }
